46a693b36f0320724b8f384b0fd4c6d18c300ff1
the_hunter <mail4thehunter@gmail.com>
2025-08-04 11:24:10 +0300

    Bump Cppcheck version to 2.18.0

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 readonly BINUTILS_VERSION="2.44"
 readonly CLANG_VERSION="20"
 readonly CMAKE_VERSION="3.31.7"
-readonly CPPCHECK_VERSION="2.17.1"
+readonly CPPCHECK_VERSION="2.18.0"
 readonly GCC_VERSION="15.1.0"
 readonly MAKE_VERSION="4.4.1"
 readonly NASM_VERSION="2.16.03"
